0c03a6062440b6b1719de07397fe718c698fb26d
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-14 12:23:57 +0100

    transactions class stores a withdrawal, test passing

--- spec/TransactionsSpec.js
+++ spec/TransactionsSpec.js
@@ -2,6 +2,7 @@ describe("Transactions", function(){
   var transactions;
   var bankaccount;
   var date1;
+  var date2;
 
   beforeEach(function(){
     transactions = new Transactions()
@@ -14,6 +15,7 @@ describe("Transactions", function(){
       }
     };
     date1 = new Date(2020, 0, 12)
+    date2 = new Date(2020, 0, 13)
   });
   
   
@@ -25,5 +27,11 @@ describe("Transactions", function(){
     spyOn(bankaccount, "getBalance").and.returnValue(1000)
     transactions.saveDeposit(date1, 1000, bankaccount.getBalance())
     expect(transactions.showTransactions()).toContain(['deposit', '12/1/2020', '1000', '1000'])
+  });
+
+  it("saves info regarding a withdrawal", function(){
+    spyOn(bankaccount, "getBalance").and.returnValue(700)
+    transactions.saveWithdrawal(date2, 300, bankaccount.getBalance())
+    expect(transactions.showTransactions()).toContain(['withdrawal', '13/1/2020', '300', '700'])
   });
 });
--- src/Transactions.js
+++ src/Transactions.js
@@ -1,21 +1,26 @@
 class Transactions {
-  constructor () {
-    this.transactions = []
+  constructor() {
+    this.transactions = [];
   }
 
-  showTransactions () {
-    return this.transactions
+  showTransactions() {
+    return this.transactions;
   }
 
   saveDeposit(date = new Date, value, updatedbalance) {
-    var transactiondate = this._getDateString(date)
-    this.transactions.push(['deposit', `${transactiondate}`, `${value}`, `${updatedbalance}`])
+    var transactiondate = this._getDateString(date);
+    this.transactions.push(['deposit', `${transactiondate}`, `${value}`, `${updatedbalance}`]);
+  }
+
+  saveWithdrawal(date = new Date, value, updatedbalance) {
+    var transactiondate = this._getDateString(date);
+    this.transactions.push(['withdrawal', `${transactiondate}`, `${value}`, `${updatedbalance}`]);
   }
 
   _getDateString(date) {
-    var day = date.getDate()
-    var month = date.getMonth() + 1
-    var year = date.getFullYear()
-    return `${day}/${month}/${year}`
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    return `${day}/${month}/${year}`;
   }
 }
