658508443ee684c2fc31a64b37db033168a5e82a
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-06 14:55:41 +0100

    refactoring: got rid of _getMonthLength + formatted date using padStart() method for the month

--- src/TransactionsList.js
+++ src/TransactionsList.js
@@ -9,26 +9,21 @@ class TransactionsList {
   }
 
   saveDeposit(date, value, updatedbalance) {
-    var transactiondate = this._getDateString(date);
+    var transactiondate = this._formattingDate(date);
     this._transactions.push(['deposit', `${transactiondate}`, `${value.toFixed(2)}`, `${updatedbalance.toFixed(2)}`]);
   }
 
   saveWithdrawal(date, value, updatedbalance) {
-    var transactiondate = this._getDateString(date);
+    var transactiondate = this._formattingDate(date);
     this._transactions.push(['withdrawal', `${transactiondate}`, `${value.toFixed(2)}`, `${updatedbalance.toFixed(2)}`]);
   }
 
-  _getDateString(date) {
+  _formattingDate(date) {
     var day = date.getDate();
     var month = date.getMonth() + 1;
     var year = date.getFullYear();
-    if (this._getMonthLength(month)) {
-      return `${day}/0${month}/${year}`;
-    }
-      return `${day}/${month}/${year}`;
-  }
-
-  _getMonthLength(month) {
-    return month.toString().length === 1;
+  
+    return `${day}/${month.toString().padStart(2, '0')}/${year}`;
+    
   }
 }
